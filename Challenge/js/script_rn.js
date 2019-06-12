3e53183fb8c2fa5bb245cbe0b1880d565b3f6c05
Ricarda Nadler <ricarda.nadler@gmx.at>
2019-06-12 15:56:04 +0200

    submit form

--- Challenge/js/script_rn.js
+++ Challenge/js/script_rn.js
@@ -1,5 +1,19 @@
 var droplist = ['Calzone', 'Diavola', 'La Toscana', 'Margerita', 'Quattro Formaggio', 'Salame'];
 droplist.forEach(function (value) {
     console.log(value);
-    document.write("<option>" + value + "</option>");
+    $("select").append("<option>" + value + "</option>");
 });
+function myFunction() {
+    var x, text;
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("textcomment").value.length;
+    // If x is Not a Number or less than one or greater than 10
+    if (x == 0) {
+        alert("Please fill out form");
+        return false;
+    }
+    else {
+        alert("Thank you for your message");
+    }
+    document.getElementById("demo").innerHTML = text;
+}
--- Challenge/js/script_rn.ts
+++ Challenge/js/script_rn.ts
@@ -2,5 +2,22 @@ let droplist: Array<string> = ['Calzone', 'Diavola', 'La Toscana','Margerita','Q
 
 droplist.forEach(function(value) {
    console.log(value);
-   $select.append("<option>"+value+"</option>");
+   $("select").append("<option>"+ value+ "</option>");
 });
+
+function myFunction() {
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("textcomment").value.length;
+
+  // If x is Not a Number or less than one or greater than 10
+  if (x == 0) {
+         alert("Please fill out form");      
+         return false;   
+     } else {
+    alert("Thank you for your message");
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
